2cc5fd9f8b0e5b5cdff08d4585def5d9dcb31330
Jake Dibble <jdibble@bsu.edu>
2020-09-18 12:54:10 -0400

    Remove extra label

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -4,13 +4,11 @@ extends Node2D
 
 var gameplay_scene = preload("res://src/Level.tscn").instance()
 onready var ButtonNode = $HUD/Button
-onready var LabelNode = $HUD/Label
 onready var LogoNode = $HUD/Logo
 onready var TitleBackground = $Background
 
 func _on_Button_pressed():
 	ButtonNode.hide()
-	LabelNode.hide()
 	LogoNode.hide()
 	TitleBackground.free()
 	get_tree().get_root().add_child(gameplay_scene)
